687457db9f1e63a6418c35e7258424eb41ef3e5a
Mino_Fe <86613190+hanminho0426@users.noreply.github.com>
2021-11-17 15:25:45 +0900

    수정

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,10 +1,51 @@
-export default function List() {
-  return (
-    <div>
-      <h1>List</h1>
-      <button onClick={click}>logout</button>
-    </div>
-  );
+import { Button, PageHeader, Table } from "antd";
+import { useEffect } from "react";
+import { BookType } from "../types";
+import Layout from "./Layout";
 
-  function click() {}
+interface ListProps {
+  books: BookType[] | null;
+  loading: boolean;
+  getBooks: () => void;
 }
+
+const List: React.FC<ListProps> = ({ books, loading, getBooks }) => {
+  useEffect(() => {
+    getBooks();
+  }, [getBooks]);
+
+  const goAdd = () => {};
+  const logout = () => {};
+  return (
+    <Layout>
+      <PageHeader
+        title={<div>Book List</div>}
+        extra={[
+          <Button key="2" type="primary" onClick={goAdd}>
+            Add Book
+          </Button>,
+          <Button key="1" type="primary" onClick={logout}>
+            Logout
+          </Button>,
+        ]}
+      />
+      <Table
+        dataSource={[]}
+        columns={[
+          {
+            title: "Book",
+            dataIndex: "book",
+            key: "book",
+            render: () => <div>book</div>,
+          },
+        ]}
+        loading={books === null || loading}
+        showHeader={false}
+        rowKey="bookId"
+        pagination={false}
+      />
+    </Layout>
+  );
+};
+
+export default List;
